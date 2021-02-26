9a7675da144347e6920c386151e6fa6bd8118209
maimoy <antonis.kalodimos@gmail.com>
2021-02-26 04:51:35 +0200

    ch3

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #local
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +127,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'accounts.CustomUser'
